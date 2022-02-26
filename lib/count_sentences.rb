cf531af239c1a2f4f8d19e6022bd1ff2d088fced
Daniel Williams <danielpaulwill@gmail.com>
2022-02-25 19:17:45 -0500

    commit tree 118f3ab141f9496a5193f562eebecf4b4d9ef960

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # if self == ""
+    #   0
+    if !!(self =~ /[!?.]/)
+      sentence = self.split(/[!?.]/)
+      sentence_arr = sentence.filter do |word|
+        if word != ""
+        word
+        end
+      end
+        sentence_num = sentence_arr.count
+        sentence_num
+    elsif
+      sentence_num = 0
+      sentence_num
+  end
+  # binding.pry
+  sentence_num
   end
 end
